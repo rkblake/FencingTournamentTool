5135c0314fc476f810255c164af41d5c98d090d8
Randall Blake <rkb.kirby@gmail.com>
2018-08-09 20:48:17 -0500

    fixed email

--- app/email.py
+++ app/email.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_mail import Message
-from app import mail
+from app import app, mail
 from threading import Thread
 
 def send_async_email(app, msg):
--- app/routes.py
+++ app/routes.py
@@ -866,4 +866,4 @@ def reset_password(token):
         db.session.commit()
         flash('Your password has been reset.')
         return redirect(url_for('login'))
-    return render_template('reset_password.html', form=form)
+    return render_template('reset-password.html', form=form)
