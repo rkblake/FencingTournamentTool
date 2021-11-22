60925a575d40daf51def32b007281ccc16adaccd
Randall Blake <rkb.kirby@gmail.com>
2021-11-21 22:04:11 -0500

    update dependencies

--- fencingtournamenttool.py
+++ fencingtournamenttool.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import User
+from app.models import User, Event, Pool, Fencer
 
 @app.shell_context_processor
 def make_shell_context():
@@ -8,3 +8,6 @@ def make_shell_context():
             'Event': Event,
             'Pool': Pool,
             'Fencer': Fencer}
+
+if __name__ == '__main__':
+    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
